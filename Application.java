86c9bc3b70f83a494570e2d6baee3ad2c959b7d4
mkuehl <maxmimilianlkuehl@web.de>
2015-10-21 09:46:28 +0200

    Variant03

--- Application.java
+++ Application.java
@@ -8,12 +8,36 @@ class  Application {
 
 	
 
+	 private void  nextDay__wrappee__BankAccount  () {
+	}
+
+	
+	
+	/*@
+	 @ ensures \original 
+	 @   && (account.balance >= 0 ==> account.interest >= \old(account.interest)) 
+	 @   && (account.balance <= 0 ==> account.interest <= \old(account.interest));
+	 @*/
 	void nextDay() {
+		nextDay__wrappee__BankAccount();
+		account.interest += account.calculateInterest();
 	}
 
 	
 
+	 private void  nextYear__wrappee__BankAccount  () {
+	}
+
+	
+
+	/*@
+	 @ ensures account.balance == \old(account.balance) + \old(account.interest) 
+	 @   && account.interest == 0;
+	 @*/
 	void nextYear() {
+		nextYear__wrappee__BankAccount();
+		account.balance += account.interest;
+		account.interest = 0;
 	}
 
 
