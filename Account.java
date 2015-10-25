9d949ac7b544916905d8a1978aa5c5182d035187
mkuehl <maxmimilianlkuehl@web.de>
2015-10-25 10:52:50 +0100

    Variant22

--- Account.java
+++ Account.java
@@ -73,6 +73,16 @@ class  Account {
 	
 
 	/*@
+	 @ requires daysLeft >= 0;
+	 @ ensures calculateInterest() >= 0 ==> \result >= interest;
+	 @*/
+	/*@ pure @*/ int estimatedInterest(int daysLeft) {
+		return interest + daysLeft * calculateInterest();
+	}
+
+	
+
+	/*@
 	 @ requires amount >= 0;
 	 @ ensures balance >= amount <==> \result;
 	 @*/
